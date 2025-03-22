4bb4c9f6461262002f6694ce8a7e18587affc273
fff <3010234291@qq.com>
2025-03-22 14:13:27 +0800

    ccc

--- modulesJavalib/src/main/java/com/fan/collect/java/Test.java
+++ modulesJavalib/src/main/java/com/fan/collect/java/Test.java
@@ -3,6 +3,9 @@ package com.fan.collect.java;
 import java.util.concurrent.Semaphore;
 
 public class Test {
+
+    int c;
+    
     public static void main(String[] args) {
 
         Integer a = 100;
